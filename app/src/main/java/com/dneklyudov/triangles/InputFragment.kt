8abd989788e0422605809f69a7dc307635a1e168
dneklyudov <dn@verdin.ru>
2025-03-13 04:02:47 +0300

    LiveData. Мелкие правки

--- app/src/main/java/com/dneklyudov/triangles/InputFragment.kt
+++ app/src/main/java/com/dneklyudov/triangles/InputFragment.kt
@@ -1,13 +1,15 @@
 package com.dneklyudov.triangles
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.dneklyudov.triangles.databinding.FragmentInputBinding
-import androidx.lifecycle.ViewModelProvider
 
 class InputFragment : Fragment() {
 
@@ -16,25 +18,66 @@ class InputFragment : Fragment() {
 
     lateinit var viewModel: InputViewModel
 
-    val len1: Int = 0
-    val len2: Int = 0
-    val len3: Int = 0
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         _binding = FragmentInputBinding.inflate(inflater, container, false)
         val view = binding.root
         viewModel = ViewModelProvider(this).get(InputViewModel::class.java)
 
-        binding.button.setOnClickListener {
-            if (viewModel.areValuesCorrect()) {
-                val action =
-                    InputFragmentDirections.actionInputFragmentToResultFragment(viewModel.resultMessage())
-                view.findNavController().navigate(action)
+        binding.length1.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable) {
+                viewModel.changeLen1(s.toString())
             }
+        })
+
+        binding.length2.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable) {
+                viewModel.changeLen2(s.toString())
+            }
+        })
+
+        binding.length3.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable) {
+                viewModel.changeLen3(s.toString())
+            }
+        })
+
+        viewModel.len1.observe(viewLifecycleOwner) { _ ->
+            viewModel.areValuesCorrect()
+        }
+        viewModel.len2.observe(viewLifecycleOwner) { _ ->
+            viewModel.areValuesCorrect()
+        }
+        viewModel.len3.observe(viewLifecycleOwner) { _ ->
+            viewModel.areValuesCorrect()
+        }
+
+        viewModel.errorMessage.observe(viewLifecycleOwner) { newValue ->
+            binding.errorTextView.text = newValue
+            if (newValue == "") {
+                binding.errorTextView.visibility = View.GONE
+            }
+            else {
+                binding.errorTextView.visibility = View.VISIBLE
+            }
+        }
+
+        viewModel.isButtonActive.observe(viewLifecycleOwner) { newValue ->
+            binding.button.isEnabled = newValue
+        }
+
+        binding.button.setOnClickListener {
+            val action = InputFragmentDirections.actionInputFragmentToResultFragment(viewModel.resultMessage())
+            view.findNavController().navigate(action)
         }
 
         binding.length1.setSelectAllOnFocus(true)
--- app/src/main/java/com/dneklyudov/triangles/InputViewModel.kt
+++ app/src/main/java/com/dneklyudov/triangles/InputViewModel.kt
@@ -1,16 +1,124 @@
 package com.dneklyudov.triangles
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.LiveData
 
 class InputViewModel: ViewModel() {
 
-    fun areValuesCorrect(): Boolean {
-        return true
+    private val _len1 = MutableLiveData<String>("0")
+    val len1: LiveData<String>
+        get() = _len1
+
+    private val _len2 = MutableLiveData<String>("0")
+    val len2: LiveData<String>
+        get() = _len2
+
+    private val _len3 = MutableLiveData<String>("0")
+    val len3: LiveData<String>
+        get() = _len3
+
+    private val _isButtonActive = MutableLiveData<Boolean>(false)
+    val isButtonActive: LiveData<Boolean>
+        get() = _isButtonActive
+
+    private val _errorMessage = MutableLiveData<String>("")
+    val errorMessage: LiveData<String>
+        get() = _errorMessage
+
+    private var isChanged1: Boolean = false
+    private var isChanged2: Boolean = false
+    private var isChanged3: Boolean = false
+
+    private var n1: Int = 0
+    private var n2: Int = 0
+    private var n3: Int = 0
+
+    fun areValuesCorrect() {
+
+        n1 = when (isChanged1) {
+            true -> getNumber(_len1.value.toString())
+            else -> 0
+        }
+        n2 = when (isChanged2) {
+            true -> getNumber(_len2.value.toString())
+            else -> 0
+        }
+        n3 = when (isChanged3) {
+            true -> getNumber(_len3.value.toString())
+            else -> 0
+        }
+
+        var tempErrorMessage = ""
+        var tempIsButtonActive = true
+
+        if (isChanged1 && n1 == 0) {
+            tempErrorMessage += "Введите длину первой стороны треугольника (целое положительное число).\n"
+            tempIsButtonActive = false
+        }
+        if (isChanged2 && n2 == 0) {
+            tempErrorMessage += "Введите длину второй стороны треугольника (целое положительное число).\n"
+            tempIsButtonActive = false
+        }
+        if (isChanged3 && n3 == 0) {
+            tempErrorMessage += "Введите длину третьей стороны треугольника (целое положительное число).\n"
+            tempIsButtonActive = false
+        }
+
+        if (isChanged1 && isChanged2 && isChanged3 && n1 > 0 && n2 > 0 && n3 > 0) {
+            if ((n1 + n2 <= n3) || (n1 + n3 <= n2) || (n2 + n3 <= n1)) {
+                tempIsButtonActive = false
+                tempErrorMessage += "Сумма длин двух сторон должна быть больше длины третьей стороны."
+            }
+        } else {
+            tempIsButtonActive = false
+        }
+
+        _isButtonActive.value = tempIsButtonActive
+        _errorMessage.value = tempErrorMessage.trim()
+
+    }
+
+    fun changeLen1(s: String) {
+        if (s != "" && _len1.value != s) {
+            isChanged1 = true
+            _len1.value = s
+        }
+    }
+
+    fun changeLen2(s: String) {
+        if (s != "" && _len2.value != s) {
+            isChanged2 = true
+            _len2.value = s
+        }
+    }
+
+    fun changeLen3(s: String) {
+        if (s != "" && _len3.value != s) {
+            isChanged3 = true
+            _len3.value = s
+        }
     }
 
     fun resultMessage(): String {
-        var message = ""
-        message = "Полученный треугольник равносторонний"
-        return message
+        return if ((n1 == n2) && (n2 == n3)) {
+            "Полученный треугольник равносторонний."
+        } else if ((n1 == n2) || (n2 == n3) || (n1 == n3)) {
+            "Полученный треугольник равнобедренный."
+        } else {
+            "Полученный треугольник разносторонний."
+        }
+    }
+
+    private fun getNumber(str: String): Int {
+        try {
+            val parsedInt = str.toInt()
+            if (parsedInt > 0) {
+                return parsedInt
+            }
+            return 0
+        } catch (e: NumberFormatException) {
+            return 0
+        }
     }
 }
